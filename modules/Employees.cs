fbe3b9f569be301869a6cff1adfa67ebd834d2ca
Joachim Hill-Grannec <joachim@suvault.com>
2017-09-18 13:37:19 -0700

    more CRUD ops

--- modules/Employees.cs
+++ modules/Employees.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using Nancy;
+using Nancy.ModelBinding;
 using NancyService.contexts;
+using NancyService.models;
 
 namespace NancyService.modules
 {
@@ -17,7 +19,24 @@ namespace NancyService.modules
             Get("/employees/{id}", args =>
             {
                 int empId = args.id;
-                var employee = db.Employees.First(emp => emp.Id == empId);
+                var employee = db.Employees.FirstOrDefault(emp => emp.Id == empId);
+                return Response.AsJson(employee);
+            });
+            
+            Delete("/employees/{id}", args =>
+            {
+                int empId = args.id;
+                var employee = db.Employees.FirstOrDefault(emp => emp.Id == empId);
+                db.Employees.Remove(employee);
+                db.SaveChanges();
+                return HttpStatusCode.OK;
+            });
+            
+            Post("/employees/new", args =>
+            {
+                var employee = this.Bind<Employee>();
+                db.Employees.Add(employee);
+                db.SaveChanges();
                 return Response.AsJson(employee);
             });
         }
