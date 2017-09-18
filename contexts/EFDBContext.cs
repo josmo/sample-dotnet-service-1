6d617536015d05248c71c670a1e51b3197d5f4a7
Joachim Hill-Grannec <joachim@suvault.com>
2017-09-18 13:53:34 -0700

    fix local dev

--- contexts/EFDBContext.cs
+++ contexts/EFDBContext.cs
@@ -11,9 +11,14 @@ namespace NancyService.contexts
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //TODO: look at a better lib to use
-            string host = Environment.GetEnvironmentVariable("SQL_HOST");
-            string user = Environment.GetEnvironmentVariable("SQL_USER");
-            string password = Environment.GetEnvironmentVariable("SQL_PASSWORD");
+            var envHost = Environment.GetEnvironmentVariable("SQL_HOST");
+            var envUser = Environment.GetEnvironmentVariable("SQL_USER");
+            var envPassword = Environment.GetEnvironmentVariable("SQL_PASSWORD");
+
+            var host = envHost ?? "localhost";
+            var user = envUser ?? "sa";
+            var password = envPassword ?? "yourStrong(!)Password";
+           
             optionsBuilder.UseSqlServer(@"Server=" + host + ";Database=master;User Id="+ user +";Password=" + password);
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
