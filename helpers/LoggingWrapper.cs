30fa05b28354d48926f1e4c00ef805e59babf7f1
Joachim Hill-Grannec <joachim@suvault.com>
2017-09-19 18:16:31 -0700

    add perf logging

---
+++ helpers/LoggingWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nancy;
+
+namespace NancyService.helpers
+{
+    public static class LoggingWrapper
+    {
+        public static Func<object, object> requestLog(dynamic module, Func<dynamic, Response> func)
+        {
+            return (dynamic args) =>
+            {
+                var startTime = DateTime.Now;
+                var returnValue = func(args);
+                Console.WriteLine("{0} {1} [{2}] ", module.Request.Method, module.Request.Url, (DateTime.Now - startTime).TotalMilliseconds.ToString());
+                return returnValue;
+            };
+        }
+    }
+}
--- modules/Employees.cs
+++ modules/Employees.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Nancy;
 using Nancy.ModelBinding;
 using NancyService.contexts;
+using NancyService.helpers;
 using NancyService.models;
 
 namespace NancyService.modules
@@ -10,35 +12,35 @@ namespace NancyService.modules
     {
         public Employees(EFDBContext db)
         {
-            Get("/employees", args =>
+            Get("/employees", LoggingWrapper.requestLog(this,args =>
             {
                     var employes = db.Employees.ToList();
                     return Response.AsJson(employes);
-            });
+            }));
             
-            Get("/employees/{id}", args =>
+            Get("/employees/{id}", LoggingWrapper.requestLog(this, args =>
             {
                 int empId = args.id;
                 var employee = db.Employees.FirstOrDefault(emp => emp.Id == empId);
                 return Response.AsJson(employee);
-            });
+            }));
             
-            Delete("/employees/{id}", args =>
+            Delete("/employees/{id}", LoggingWrapper.requestLog(this,args =>
             {
                 int empId = args.id;
                 var employee = db.Employees.FirstOrDefault(emp => emp.Id == empId);
                 db.Employees.Remove(employee);
                 db.SaveChanges();
                 return HttpStatusCode.OK;
-            });
+            }));
             
-            Post("/employees/new", args =>
+            Post("/employees/new", LoggingWrapper.requestLog(this,args =>
             {
                 var employee = this.Bind<Employee>();
                 db.Employees.Add(employee);
                 db.SaveChanges();
                 return Response.AsJson(employee);
-            });
+            }));
         }
     }
 
